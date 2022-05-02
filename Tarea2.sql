3996e9b4cfc3b4ea87a7a81c4ea29c11575ee34b
Inaki-FF <78442936+Inaki-FF@users.noreply.github.com>
2022-05-02 11:40:56 -0500

    Add files via upload

---
+++ Tarea2.sql
@@ -0,0 +1,33 @@
+create table superheroes (
+superheroes_name varchar(50),
+email varchar(75))
+
+insert into superheroes (superheroe_name,email)
+Values('Wanda Maximoff','wanda.maximoff@avengers.org'),
+('Pietro Maximoff','pietro@mail.sokovia.ru'),
+('Erik Lensherr','fuck_you_charles@brotherhood.of.evil.mutants.space'),
+('Charles Xavier','i.am.secretely.filled.with.hubris@xavier-school-4-gifted-youngste.'),
+('Anthony Edward Stark','iamironman@avengers.gov'),
+('Steve Rogers','americas_ass@anti_avengers'),
+('The Vision','vis@westview.sword.gov'),
+('Clint Barton','bul@lse.ye'),
+('Natasja Romanov','blackwidow@kgb.ru'),
+('Thor','god_of_thunder-^_^@royalty.asgard.gov'),
+('Logan','wolverine@cyclops_is_a_jerk.com'),
+('Ororo Monroe','ororo@weather.co'),
+('Scott Summers','o@x'),
+('Nathan Summers','cable@xfact.or'),
+('Groot','iamgroot@asgardiansofthegalaxyledbythor.quillsux'),
+('Nebula','idonthaveelektras@complex.thanos'),
+('Gamora','thefiercestwomaninthegalaxy@thanos.'),
+('Rocket','shhhhhhhh@darknet.ru')
+
+select *
+from superheroes s 
+where email !~'^.+\@.+\..+$'
+
+select * 
+from superheroes s 
+where email  not like '%@%._%'
+
+
