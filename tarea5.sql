e956955e24cb8d1aaa5eb54af269f78df09ac7a4
Inaki-FF <78442936+Inaki-FF@users.noreply.github.com>
2022-06-01 22:37:40 -0500

    Update tarea5.sql

--- tarea5.sql
+++ tarea5.sql
@@ -2,10 +2,7 @@ with movies_per_store as (select store_id, count(i.film_id) as "num pels" from i
 
 
 	nummax as (select 50/.5 as "pels por cilindro"),
-
---altura del cilindro, aquí suponemos que están pegadas todas las películas, 
--- si les quisiéramos dar una separación de 1 cm entre c/u sería algo como 
--- select 2.5*nm."pels por cilindro"-1 as "altura"  from nummax nm, el -1 porque para el techo ya no necesitas separación
+	
 	ht as (select 1.5*nm."pels por cilindro" as "altura"  from nummax nm),
 
 	rad as (select sqrt(power(30/2,2) + power(21/2,2)) as "radio")
